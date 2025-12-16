03fb35cdd10a949c6604a9376135fe8a52eebff2
Norbert Balazs <bnorb93@gmail.com>
2025-12-17 00:14:18 +0100

    added logic to download input

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 FIRST_YEAR=2015
 CURRENT_YEAR=$(date +%Y)
+CUTOFF_YEAR="2025" # from this year, there are only 12 days of puzzles instead of 25
 YEAR="$CURRENT_YEAR"
 DAY=$(date +%-d)
 PART=1
@@ -28,6 +29,14 @@ OPTIONS:
 EOF
 
   exit $exitCode
+}
+
+function error() {
+  message=$1
+
+  echo $message
+
+  exit 1
 }
 
 function validateYear() {
@@ -87,12 +96,30 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-function runDay() {
+[ -f "./.env" ] && source ./.env
+
+function getInput() {
   year=$1
   day=$2
 
+  command -v wget > /dev/null || error "Need wget to download input"
+
+  [ -n "$AOC_SESSION" ] || error "Need AOC_SESSION set up in .env file to download input"
+
+  wget --quiet --header="Cookie: session=$AOC_SESSION" -O "./input/day$day.txt" "https://adventofcode.com/$year/day/$day/input"
+}
+
+function runDay() {
+  year=$1
+  day=$2
+  lastDay=$3
+
+  [ "$day" -gt "$lastDay" ] && error "Trying to run day $day, but last day is $lastDay"
+
+  [ -f "./input/day$day.txt" ] || getInput $year $day
+
   [[ $PART == "all" ]] || [ "$PART" -eq "1" ] && ./run.sh $day 1
-  [[ $PART == "all" ]] || [ "$PART" -eq "2" ] && [ "$day" -lt "25" ] && ./run.sh $day 2
+  [[ $PART == "all" ]] || [ "$PART" -eq "2" ] && [ "$day" -lt "$lastDay" ] && ./run.sh $day 2
 }
 
 function runYear() {
@@ -103,17 +130,17 @@ function runYear() {
   [ ! -f "./run.sh" ] && echo "Not solved yet" && return
   [ -f "./build.sh" ] && ./build.sh > $LOG_OUTPUT
 
+  lastDay=25
+  [ "$year" -ge "$CUTOFF_YEAR" ] && lastDay=12
+
   if [[ $DAY == "all" ]]; then
-    lastDay=25
-    [ "$year" -ge "2025" ] && lastDay=12
-    
     for day in $(seq 1 $lastDay); do
       echo "Day $day:"
-      runDay $year $day
+      runDay $year $day $lastDay
       echo ""
     done
   else
-    runDay $year $DAY
+    runDay $year $DAY $lastDay
   fi
 
   popd > $LOG_OUTPUT
